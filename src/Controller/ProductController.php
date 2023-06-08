86048c94ba9cdab5235a9eb7fbcb5794c70650ae
neo-askin <issayamilrt@gmail.com>
2023-06-08 18:09:02 +0200

    test template

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,19 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    /**
-     * @Route("/product", name="app_product")
-     */
+    public function __construct(private readonly EntityManagerInterface $em)
+    {
+
+    }
+    #[Route('/shop', name: 'shop_index')]
     public function index(): Response
     {
+        $products = $this->em->getRepository(Product::class)->findAll();
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products
         ]);
     }
 }
